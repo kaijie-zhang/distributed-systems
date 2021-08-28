42ebc3b66b07fabf89c357fc270fc5243bec3389
Kaijie Zhang <kaijiezhang22@gmail.com>
2021-08-28 22:22:04 +0200

    remove .idea directory

--- leader-election/src/main/java/LeaderElection.java
+++ leader-election/src/main/java/LeaderElection.java
@@ -2,7 +2,7 @@ import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
 
-public class LeaderElection {
+public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT=3000;
     private ZooKeeper zookeeper;
