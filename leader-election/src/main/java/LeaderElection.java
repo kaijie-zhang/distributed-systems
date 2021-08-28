f5034af88a60bf16d21578997c859faf07594ab4
Kaijie Zhang <kaijiezhang22@gmail.com>
2021-08-28 21:37:16 +0200

    leader election project start

---
+++ leader-election/src/main/java/LeaderElection.java
@@ -0,0 +1,16 @@
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class LeaderElection {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT=3000;
+    private ZooKeeper zookeeper;
+    public static void main(String[] args){
+
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zookeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+}
